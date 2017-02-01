8f5d3a48c6c99b659f869f726f6da2a0a0678f4d
ihsoft <igor.zavoychinskiy@gmail.com>
2017-01-31 23:40:06 -0800

    Initial version

---
+++ Source/ModuleMaterialTweaker.cs
@@ -0,0 +1,114 @@
+﻿﻿// Material settings tweaker.
+// Author: igor.zavoychinskiy@gmail.com 
+// License: Public Domain.
+
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace KSPDev.PartTweaker {
+
+/// <summary>Simple module that allows overriding renderer material color property.</summary>
+public sealed class ModuleMaterialTweaker : PartModule {
+
+  /// <summary>Color property name in the material to change.</summary>
+  /// <seealso href="https://docs.unity3d.com/Manual/MaterialsAccessingViaScript.html">
+  /// Unity 3D: Material parameters</seealso>
+  [KSPField]
+  public string materialVarName = "";
+
+  /// <summary>Name of the color to present in the editor.</summary>
+  [KSPField]
+  public string varDisplayName = "";
+
+  /// <summary>Specifies if Alpha component is allowed to be customized.</summary>
+  [KSPField]
+  public bool showAlpha = true;
+
+  /// <summary>Specifies if RGB components are allowed to be customized.</summary>
+  [KSPField]
+  public bool showRGB = true;
+
+  /// <summary>Specifies if color adjusting settings should eb available in flight.</summary>
+  [KSPField]
+  public bool allowInFlight = false;
+
+  /// <summary>R component of the color.</summary>
+  [KSPField(isPersistant = true, guiName = "R", guiActiveEditor = true),
+   UI_FloatRange(stepIncrement = 0.05f, maxValue = 1f, minValue = 0f)]
+  public float colorR = 1f;
+
+  /// <summary>G component of the color.</summary>
+  [KSPField(isPersistant = true, guiName = "G", guiActiveEditor = true),
+   UI_FloatRange(stepIncrement = 0.05f, maxValue = 1f, minValue = 0f)]
+  public float colorG = 1f;
+
+  /// <summary>B component of the color.</summary>
+  [KSPField(isPersistant = true, guiName = "B", guiActiveEditor = true),
+   UI_FloatRange(stepIncrement = 0.05f, maxValue = 1f, minValue = 0f)]
+  public float colorB = 1f;
+  
+  /// <summary>Alpha component of the color.</summary>
+  [KSPField(isPersistant = true, guiName = "Alpha", guiActiveEditor = true),
+   UI_FloatRange(stepIncrement = 0.05f, maxValue = 1f, minValue = 0f)]
+  public float colorA = 1f;
+
+  /// <summary>Model's material.</summary>
+  protected Material material {
+    get {
+      if (!_material) {
+        _material = part.FindModelComponent<Renderer>().material;
+      }
+      return _material;
+    }
+  }
+  Material _material;
+
+  #region PartModule overrides
+  /// <inheritdoc/>
+  public override void OnInitialize() {
+    // Use initialize instead of Start() due to the latter is not called in the editor.
+    base.OnInitialize();
+    UpdateColor();
+    SetColorComponent(Fields["colorR"], varDisplayName + " R");
+    SetColorComponent(Fields["colorG"], varDisplayName + " G");
+    SetColorComponent(Fields["colorB"], varDisplayName + " B");
+    SetAlphaComponent(Fields["colorA"], varDisplayName + " Alpha");
+  }
+
+  /// <inheritdoc/>
+  public override void OnAwake() {
+    base.OnAwake();
+    if (HighLogic.LoadedSceneIsEditor || allowInFlight) {
+      if (showRGB) {
+        Fields["colorR"].OnValueModified += (x => UpdateColor());
+        Fields["colorG"].OnValueModified += (x => UpdateColor());
+        Fields["colorB"].OnValueModified += (x => UpdateColor());
+      }
+      if (showAlpha) {
+        Fields["colorA"].OnValueModified += (x => UpdateColor());
+      }
+    }
+  }
+  #endregion
+
+  #region Local utility methods
+  void UpdateColor() {
+    material.SetColor(materialVarName, new Color(colorR, colorB, colorG, colorA));
+  }
+
+  void SetColorComponent(BaseField field, string guiName) {
+    field.guiName = guiName;
+    field.guiActiveEditor = showRGB;
+    field.guiActive = showRGB && allowInFlight;
+  }
+
+  void SetAlphaComponent(BaseField field, string guiName) {
+    field.guiName = guiName;
+    field.guiActiveEditor = showAlpha;
+    field.guiActive = showAlpha && allowInFlight;
+  }
+  #endregion
+}
+
+}  // namespace
---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("KSPDev: PartTweaker")]
+[assembly: AssemblyDescription ("Various modules for tweaking third-party parts")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("IHSoft")]
+[assembly: AssemblyProduct ("KSPDev_PartTweaker")]
+[assembly: AssemblyCopyright ("Public Domain")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible (false)]
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion ("1.0.0")]
+[assembly: AssemblyInformationalVersion ("1.0.0 for KSP v1.2")]
+[assembly: Guid ("538f23dc-dfc0-4a78-a234-4af3269ae0fa")]
+[assembly: KSPAssembly ("KSPDev_PartTweaker", 1, 0)]
+[assembly: AssemblyFlags (AssemblyNameFlags.PublicKey | AssemblyNameFlags.EnableJITcompileOptimizer)]
